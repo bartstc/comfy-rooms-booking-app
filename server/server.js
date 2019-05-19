e03d9910b11e9f9eef8d724c5e0d04a04bd03b5b
Bart <bartst010395@gmail.com>
2019-05-19 23:21:02 +0200

    Init commit, basic server setup

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// const passport = require('passport');
+const db = require('./config/keys').mongoURI;
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+
+// Passport middleware
+// app.use(passport.initialize());
+
+// Passport Config
+// require('./config/passport')(passport);
+
+// Use Routes
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(port);
+    console.log('DB connected')
+  })
+  .catch(err => {
+    console.log(err);
+  });

d572b8b373dd1a6b323fa4e50ea439cdf92f2a9a
Bartosz Stolc <bartst010395@gmail.com>
2019-06-23 18:19:35 +0200

    prepared to deploy

---
+++ server/config/keys_prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  adminId1: process.env.ADMIN_ID_1,
+  cloudName: process.env.CLOUD_NAME,
+  cloudAPIKey: process.env.CLOUD_API_KEY,
+  cloudAPISecret: process.env.CLOUD_API_SECRET,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+};
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const db = require('./config/keys').mongoURI;
@@ -32,6 +33,14 @@ app.use('/api/profiles', profiles);
 app.use('/api/hotels', hotels);
 app.use('/api/rooms', rooms);
 app.use('/api/admin', admin);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  })
+};
 
 mongoose
   .connect(db, {
